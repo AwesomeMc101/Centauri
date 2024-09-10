a723965a343f7a213f4882331358293f61a63d06
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-09-09 23:01:18 -0500

    Create stack.hpp

---
+++ Data/stack.hpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include "heap.hpp"
+
+class Stack {
+private:
+	std::vector<CentauriObj> stack{};
+
+	const unsigned int register_size = 4;
+	CentauriObj registers[4];
+	CentauriObj* r_ptr;
+	int rc_ptr;
+public:
+	void pop(CentauriObj*);
+	void push(CentauriObj);
+
+	CentauriObj* get_r(int s = 0); //get last written to register
+	CentauriObj* get_a_r(); //get accessible register
+
+};
