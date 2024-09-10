5b4b455a57ec6036023353ac5232ec3c907e2a9f
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-09-09 23:00:57 -0500

    Create tokenizer.hpp

---
+++ Generation/tokenizer.hpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <string>
+
+typedef enum {
+	CHAR,
+	QUOTE,
+	INT,
+	PERIOD, //for floats
+
+	ASTERISK,
+	PLUS,
+	MINUS,
+	FORWARD_SLASH,
+	PERCENT,
+	CARET,
+	EQUALS,
+	
+	GT,
+	LT,
+
+	SPACE,
+
+	OPENPARA,
+	CLOSEPARA,
+	OPENBRACKET,
+	CLOSEBRACKET
+} TokenType;
+
+typedef struct Token {
+	TokenType type;
+	char c;
+};
+
+class Tokenized_Data {
+public:
+	std::vector<Token> tokens{};
+};
+
+Tokenized_Data get_tokens(std::string line);
